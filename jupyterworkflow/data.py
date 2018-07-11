701ca424722051e099cd94dd18b42bc85d3dc757
shaybensasson <shaybensasson@gmail.com>
2018-07-11 14:08:35 +0300

    refactored download

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -28,8 +28,18 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL,
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
 
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    #Before:
+    #data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
     #make parse string of Date and make it an index
+
+    #After: x20 faster
+    # look at http://strftime.org/
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index) #infer it automatically takes alot of time x10 at least
+
     
     data.columns = ['East', 'West']
     data['Total'] = data['West'] + data['East']
